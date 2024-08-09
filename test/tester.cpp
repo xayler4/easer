b9cb70e6433de3da651a65208b3f76977488592a
Andrea Pastore <andrea.pastore04@outlook.com>
2024-08-09 16:11:45 +0200

    implemented external type serialization declaration

--- test/tester.cpp
+++ test/tester.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <cstdio>
 
-constexpr char test_file_names[][128] = {"first_test.dat", "second_test.dat", "third_test.dat", "fourth_test.dat", "fifth_test.dat"};
+constexpr char test_file_names[][128] = {"first_test.dat", "second_test.dat", "third_test.dat", "fourth_test.dat", "fifth_test.dat", "sixth_test.dat", "seventh_test.dat"};
 
 struct Config {
 	Config() {
@@ -27,7 +27,7 @@ struct TestRecord {
 	FIELD(int, a);
 	FIELD(bool, b);
 	FIELD(char, c);
-	END;
+	END();
 };
 
 BOOST_AUTO_TEST_CASE(first_test) {
@@ -75,7 +75,7 @@ struct TestRecordDerivedSerializable : public TestRecord {
 	FIELD(int, d);
 	FIELD(bool, e);
 	FIELD(char, f);
-	END;
+	END();
 };
 
 BOOST_AUTO_TEST_CASE(third_test) {
@@ -103,7 +103,7 @@ struct TestRecordDerivedTwiceSerializable : public TestRecordDerivedSerializable
 	FIELD(int, g);
 	FIELD(bool, h);
 	FIELD(char, i);
-	END;
+	END();
 };
 
 BOOST_AUTO_TEST_CASE(fourth_test) {
@@ -135,7 +135,7 @@ struct TestRecordDerivedTwiceSerializableMember : public TestRecordDerivedSerial
 	FIELD(TestRecord, test_record_member);
 	FIELD(bool, h);
 	FIELD(char, i);
-	END;
+	END();
 };
 
 BOOST_AUTO_TEST_CASE(fifth_test) {
@@ -164,4 +164,52 @@ BOOST_AUTO_TEST_CASE(fifth_test) {
 
 	BOOST_TEST(record.h == in_record.h);
 	BOOST_TEST(record.i == in_record.i);
+}
+
+struct Vec2 {
+	int x;
+	int y;
+};
+
+TYPE(Vec2, v, v.x, v.y)
+
+BOOST_AUTO_TEST_CASE(sixth_test) {
+	Vec2 record{12, 97};
+	Vec2 in_record{};
+	{
+		std::ofstream file(test_file_names[5]);
+		easer::Serializer::serialize(record, file);
+	}
+	{
+		std::ifstream file(test_file_names[5]);
+		easer::Serializer::deserialize(in_record, file);
+	}
+
+	BOOST_TEST(record.x == in_record.x);
+	BOOST_TEST(record.y == in_record.y);
+}
+
+struct TestStructVec2 {
+	BEGIN();
+	FIELD(Vec2, a);
+	FIELD(Vec2, b);
+	END();
+};
+
+BOOST_AUTO_TEST_CASE(seventh_test) {
+	TestStructVec2 record{{ 12, 97 }, {48, 7}};
+	TestStructVec2 in_record{};
+	{
+		std::ofstream file(test_file_names[6]);
+		easer::Serializer::serialize(record, file);
+	}
+	{
+		std::ifstream file(test_file_names[6]);
+		easer::Serializer::deserialize(in_record, file);
+	}
+
+	BOOST_TEST(record.a.x == in_record.a.x);
+	BOOST_TEST(record.a.y == in_record.a.y);
+	BOOST_TEST(record.b.x == in_record.b.x);
+	BOOST_TEST(record.b.y == in_record.b.y);
 }
